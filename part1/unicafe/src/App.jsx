4fc8e906ae15d47c791afa76f635881bdab48b1f
Tomasz Nguyen <tomasz.nguyen@bain.com>
2026-01-12 16:58:35 +0100

    part 1 unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,9 +10,10 @@ const Button = (props) => {
 
 const StatisticLine = ({ text, value }) => {
     return (
-        <div>
-            {text} {value}
-        </div>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 }
 
@@ -34,12 +35,14 @@ const Statistics = (props) => {
     return (
         <>
             <h1>statistics</h1>
-            <StatisticLine text='good' value={props.good} />
-            <StatisticLine text='neutral' value={props.neutral} />
-            <StatisticLine text='bad' value={props.bad} />
-            <StatisticLine text='all' value={all} />
-            <StatisticLine text='average' value={average} />
-            <StatisticLine text='positive' value={positive} />
+            <table>
+                <StatisticLine text='good' value={props.good} />
+                <StatisticLine text='neutral' value={props.neutral} />
+                <StatisticLine text='bad' value={props.bad} />
+                <StatisticLine text='all' value={all} />
+                <StatisticLine text='average' value={average} />
+                <StatisticLine text='positive' value={positive} />
+            </table>
         </>
     )
 }
